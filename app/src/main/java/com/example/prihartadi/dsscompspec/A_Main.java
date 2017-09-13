cedffd65410c571191bc00090d13f07724c7aeae
prihartadi <31931613+prihartadi@users.noreply.github.com>
2017-09-13 23:49:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/prihartadi/dsscompspec/A_Main.java
@@ -0,0 +1,13 @@
+package com.example.prihartadi.dsscompspec;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class A_Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a__main);
+    }
+}
